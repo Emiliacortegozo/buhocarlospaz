59ac7726bc581036b7130ade43391929ea89aaa9
emiliacortegozo <mecortegozo@gmail.com>
2022-12-13 09:25:15 -0300

    to gcp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from "react";
+import header from "bootstrap"
+import Redes from "./components/redes"
+import CarouselEx from './components/carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>HOLIS</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+       <div className='App'>
+      DEPARTAMENTOS en
+      VILLA CARLOS PAZ
+      </div>
+      <CarouselEx />
+ 
+    <Redes />
+    
+     
+     <a
+        href="https://wa.me/5493541677768"
+        className="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+        text= "chatea con nosotros"
+       title='chatea con nosotros'
+       
+      >
+        <i className="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
+     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "react-bootstrap"
 
+import CarouselEx from './components/carousel';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+   
   </React.StrictMode>
 );
 
